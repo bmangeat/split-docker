c325608e7b142da73cb7bf2936686a0c8b731a2e
Brice MANGEAT <b.mangeat@thecodingmachine.com>
2023-03-09 19:43:36 +0100

    :zap: Setup cache

--- app/back/src/app.module.ts
+++ app/back/src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [CacheModule.register({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
